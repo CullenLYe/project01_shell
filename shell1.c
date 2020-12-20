13d3609e89f5718d8d959ec46aa31a36ba25b437
Jacky <jchen10@stuy.edu>
2020-12-20 03:17:09 -0500

    proto pipe

--- shell1.c
+++ shell1.c
@@ -22,9 +22,25 @@ char ** semi_sep(char *line, char *delimiter) {
 
 void runcmd(char *command) { // Runs each respective command.
     int status;
+    // printf("%s\n",command);
     char **args = semi_sep(command, " ");
     if (!strcmp(args[0], "exit")) exit(0);
-    else if (!strcmp(args[0], "cd")) 
+    else if(strstr(command,"|") != NULL){ 
+      // anything with spaces won't work 
+      // ls|wc does work
+      // example: cat ayy.txt| grep -v a |sort -r
+      int f = fork();
+      if(f) wait(&status);
+      else{
+      FILE *fp;
+      char buff[80];
+      fp = popen(command, "r");
+      fgets( buff, sizeof(buff), fp);
+      printf("%s", buff);
+      pclose(fp);
+      exit(0);}
+    }
+    else if (!strcmp(args[0], "cd"))
       if (chdir(args[1]) == -1) printf("Invalid directory.  MSG: %s\n", strerror(errno));
       else ;
     else {
